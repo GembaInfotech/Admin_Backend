d02b8bad34d202e2b6d37c1dfbb7d0bfa9645f22
SurabhiYadav1 <s.yadav@gembainfotech.com>
2024-05-24 09:56:28 +0530

    m

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"));
+app.use(express.static("dist"));
 app.use(cookieParser());
 app.use(morgan("dev"));
 
